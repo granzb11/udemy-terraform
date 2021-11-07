4b788c8a45f31ab3dc18563b2d259694f30545f7
gustavo <granzb11@hotmail.com>
2021-11-07 14:13:22 -0500

    updates

--- vars.tf
+++ vars.tf
@@ -1,28 +1,22 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
-
 variable "AWS_REGION" {
-  default = "eu-west-1"
+    default = "us-east-1"
 }
-
 variable "AMIS" {
-  type = map(string)
-  default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
-  }
+    type = map
+    default = {
+        us-east-1 = "ami-0133407e358cc1af0"
+        us-east-2 = "ami-01685d240b8fbbfeb"
+        us-west-2 = "ami-079e7a3f57cc8e0d0"
+    }
 }
-
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
+    default = "mykey"
 }
-
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+    default = "mykey.pub"
 }
-
 variable "INSTANCE_USERNAME" {
-  default = "ubuntu"
+    default = "ubuntu"
 }
-
